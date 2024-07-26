ad791fc7571a5ee7333b59fa58215f9ca5517510
Matyas-Mattanelli <91661261+Matyas-Mattanelli@users.noreply.github.com>
2024-07-26 08:49:44 +0200

    Animating the name

---
+++ animatedName.js
@@ -0,0 +1,21 @@
+// Find the title to be changed
+const h1 = document.querySelector('h1.name');
+
+// Get the title text
+const title = h1.textContent;
+
+// Define the last current index shown
+let letterIndex = 0;
+
+// Define a function showing the title up to the current index and repeatedly calling itself
+function showTitle() {
+    h1.textContent = title.slice(0, letterIndex)
+    letterIndex++
+    if (letterIndex === (title.length + 1)) {
+        letterIndex = 0
+    }
+    setTimeout(showTitle, 300)
+}
+
+// Call the function
+showTitle()
